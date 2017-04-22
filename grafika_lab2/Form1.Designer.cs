74dbb34035952a848d50c55b895ef85d15ae0f02
Tomasz Zapiórkowski <zapiorkowski@gmail.com>
2017-04-22 16:02:30 +0200

    Add files via upload

---
+++ grafika_lab2/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace grafika_lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.DrawCircle = new System.Windows.Forms.Button();
+            this.DrawSpiral = new System.Windows.Forms.Button();
+            this.DrawSpirale = new System.Windows.Forms.Button();
+            this.Draw4Sprial = new System.Windows.Forms.Button();
+            this.Draw16 = new System.Windows.Forms.Button();
+            this.Draw19 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.PictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(767, 599);
+            this.PictureBox1.TabIndex = 0;
+            this.PictureBox1.TabStop = false;
+            this.PictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // DrawCircle
+            // 
+            this.DrawCircle.Location = new System.Drawing.Point(787, 12);
+            this.DrawCircle.Name = "DrawCircle";
+            this.DrawCircle.Size = new System.Drawing.Size(180, 39);
+            this.DrawCircle.TabIndex = 1;
+            this.DrawCircle.Text = "Koło";
+            this.DrawCircle.UseVisualStyleBackColor = true;
+            this.DrawCircle.Click += new System.EventHandler(this.DrawCircle_Click);
+            // 
+            // DrawSpiral
+            // 
+            this.DrawSpiral.Location = new System.Drawing.Point(787, 57);
+            this.DrawSpiral.Name = "DrawSpiral";
+            this.DrawSpiral.Size = new System.Drawing.Size(180, 39);
+            this.DrawSpiral.TabIndex = 2;
+            this.DrawSpiral.Text = "Elipsax2";
+            this.DrawSpiral.UseVisualStyleBackColor = true;
+            this.DrawSpiral.Click += new System.EventHandler(this.DrawSpiral_Click);
+            // 
+            // DrawSpirale
+            // 
+            this.DrawSpirale.Location = new System.Drawing.Point(787, 102);
+            this.DrawSpirale.Name = "DrawSpirale";
+            this.DrawSpirale.Size = new System.Drawing.Size(180, 39);
+            this.DrawSpirale.TabIndex = 3;
+            this.DrawSpirale.Text = "Spirala";
+            this.DrawSpirale.UseVisualStyleBackColor = true;
+            this.DrawSpirale.Click += new System.EventHandler(this.DrawSpirale_Click);
+            // 
+            // Draw4Sprial
+            // 
+            this.Draw4Sprial.Location = new System.Drawing.Point(787, 147);
+            this.Draw4Sprial.Name = "Draw4Sprial";
+            this.Draw4Sprial.Size = new System.Drawing.Size(180, 39);
+            this.Draw4Sprial.TabIndex = 4;
+            this.Draw4Sprial.Text = "4Spirale";
+            this.Draw4Sprial.UseVisualStyleBackColor = true;
+            this.Draw4Sprial.Click += new System.EventHandler(this.Draw4Sprial_Click);
+            // 
+            // Draw16
+            // 
+            this.Draw16.Location = new System.Drawing.Point(787, 192);
+            this.Draw16.Name = "Draw16";
+            this.Draw16.Size = new System.Drawing.Size(180, 39);
+            this.Draw16.TabIndex = 5;
+            this.Draw16.Text = "1.6";
+            this.Draw16.UseVisualStyleBackColor = true;
+            this.Draw16.Click += new System.EventHandler(this.Draw16_Click);
+            // 
+            // Draw19
+            // 
+            this.Draw19.Location = new System.Drawing.Point(787, 237);
+            this.Draw19.Name = "Draw19";
+            this.Draw19.Size = new System.Drawing.Size(180, 39);
+            this.Draw19.TabIndex = 6;
+            this.Draw19.Text = "1.9";
+            this.Draw19.UseVisualStyleBackColor = true;
+            this.Draw19.Click += new System.EventHandler(this.Draw19_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(979, 602);
+            this.Controls.Add(this.Draw19);
+            this.Controls.Add(this.Draw16);
+            this.Controls.Add(this.Draw4Sprial);
+            this.Controls.Add(this.DrawSpirale);
+            this.Controls.Add(this.DrawSpiral);
+            this.Controls.Add(this.DrawCircle);
+            this.Controls.Add(this.PictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PictureBox1;
+        private System.Windows.Forms.Button DrawCircle;
+        private System.Windows.Forms.Button DrawSpiral;
+        private System.Windows.Forms.Button DrawSpirale;
+        private System.Windows.Forms.Button Draw4Sprial;
+        private System.Windows.Forms.Button Draw16;
+        private System.Windows.Forms.Button Draw19;
+    }
+}
+
---
+++ grafika_lab2/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace grafika_lab2
+{
+    public partial class Form1 : Form
+    {
+        private System.Drawing.Graphics g;
+        private System.Drawing.Pen pen1 = new System.Drawing.Pen(Color.Blue, 2);
+        public Form1()
+        {
+            InitializeComponent();
+            g = PictureBox1.CreateGraphics();
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DrawCircle_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            float SrodekX = 250;
+            float SrodekY = 250;
+            int LiczbaKrokow = 100;
+            float R = 100;
+            float Alfa = 0;
+            float Dalfa = 5;//(float)0.0628;
+            for(int i =0; i<LiczbaKrokow; i++)
+            {
+                g.DrawLine(pen1, SrodekX + R * (float)Math.Cos(Alfa + Dalfa * i), SrodekY + R * (float)Math.Sin(Alfa + Dalfa * i), SrodekX + R * (float)Math.Cos(Alfa + Dalfa * (i + 1)), SrodekY + R * (float)Math.Sin(Alfa + Dalfa * (i + 1)));
+            }
+        }
+
+        private void DrawSpiral_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            float SrodekX = 250;
+            float SrodekY = 250;
+            int LiczbaKrokow = 100;
+            float R1 = 100;
+            float R2 = 50;
+            float Alfa = 0;
+            float Dalfa = (float)0.0628;
+            for (int j = 0; j < 4; j++)
+            {
+                for (int i = 0; i < LiczbaKrokow; i++)
+                {
+                    g.DrawLine(pen1, SrodekX + R1 * (float)Math.Cos(Alfa + Dalfa * i), SrodekY + R2 * (float)Math.Sin(Alfa + Dalfa * i), SrodekX + R1 * (float)Math.Cos(Alfa + Dalfa * (i + 1)), SrodekY + R2 * (float)Math.Sin(Alfa + Dalfa * (i + 1)));
+                    g.DrawLine(pen1, SrodekX + R2 * (float)Math.Cos(Alfa + Dalfa * i), SrodekY + R1 * (float)Math.Sin(Alfa + Dalfa * i), SrodekX + R2 * (float)Math.Cos(Alfa + Dalfa * (i + 1)), SrodekY + R1 * (float)Math.Sin(Alfa + Dalfa * (i + 1)));
+                }
+                R1 += 50;
+                R2 += 50;
+            }
+            for (int i = 0; i < LiczbaKrokow; i++)
+            {
+                g.DrawLine(pen1, SrodekX + (R1-50) * (float)Math.Cos(Alfa + Dalfa * i), SrodekY + (R1-50) * (float)Math.Sin(Alfa + Dalfa * i), SrodekX + (R1-50) * (float)Math.Cos(Alfa + Dalfa * (i + 1)), SrodekY + (R1-50) * (float)Math.Sin(Alfa + Dalfa * (i + 1)));
+            }
+        }
+
+        private void DrawSpirale_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            float SrodekX = 250;
+            float SrodekY = 250;
+            int N = 5;
+            float R = 100;
+            float Alfa = 0;
+            float Dalfa = (float)0.0628;// 2*(float)Math.PI/N;
+            float Nn = N * 100;
+            float Rr = R / Nn;
+            float Rb = 10;
+            for(int i = 1; i<Nn; i++)
+            {
+                Rb = Rr * i;
+                g.DrawLine(pen1, SrodekX + Rb * (float)Math.Cos(Alfa + i * Dalfa), SrodekY + Rb * (float)Math.Sin(Alfa + i * Dalfa), SrodekX + Rb * (float)Math.Cos(Alfa + (i + 1) * Dalfa), SrodekX + Rb * (float)Math.Sin(Alfa + (i + 1) * Dalfa));
+            }
+
+        }
+
+        private void Draw4Sprial_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            float SrodekX = 250;
+            float SrodekY = 250;
+            int N = 5;
+            float R = 100;
+            float Alfa1 = 0;
+            float Alfa2 = (float)67.5;
+            float Alfa3 = 2*(float)67.5;
+            float Alfa4 = 3*(float)67.5;
+            float Dalfa = (float)0.0628;// 2*(float)Math.PI/N;
+            float Nn = N * 100;
+            float Rr = R / Nn;
+            float Rb = 10;
+            for (int i = 1; i < Nn; i++)
+            {
+                Rb = Rr * i;
+                g.DrawLine(pen1, SrodekX + Rb * (float)Math.Cos(Alfa1 + i * Dalfa), SrodekY + Rb * (float)Math.Sin(Alfa1 + i * Dalfa), SrodekX + Rb * (float)Math.Cos(Alfa1 + (i + 1) * Dalfa), SrodekX + Rb * (float)Math.Sin(Alfa1 + (i + 1) * Dalfa));
+                g.DrawLine(pen1, SrodekX + Rb * (float)Math.Cos(Alfa2 + i * Dalfa), SrodekY + Rb * (float)Math.Sin(Alfa2 + i * Dalfa), SrodekX + Rb * (float)Math.Cos(Alfa2 + (i + 1) * Dalfa), SrodekX + Rb * (float)Math.Sin(Alfa2 + (i + 1) * Dalfa));
+                g.DrawLine(pen1, SrodekX + Rb * (float)Math.Cos(Alfa3 + i * Dalfa), SrodekY + Rb * (float)Math.Sin(Alfa3 + i * Dalfa), SrodekX + Rb * (float)Math.Cos(Alfa3 + (i + 1) * Dalfa), SrodekX + Rb * (float)Math.Sin(Alfa3 + (i + 1) * Dalfa));
+                g.DrawLine(pen1, SrodekX + Rb * (float)Math.Cos(Alfa4 + i * Dalfa), SrodekY + Rb * (float)Math.Sin(Alfa4 + i * Dalfa), SrodekX + Rb * (float)Math.Cos(Alfa4 + (i + 1) * Dalfa), SrodekX + Rb * (float)Math.Sin(Alfa4 + (i + 1) * Dalfa));
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                g.DrawLine(pen1, SrodekX + R * (float)Math.Cos(Alfa1 + Dalfa * i), SrodekY + R * (float)Math.Sin(Alfa1 + Dalfa * i), SrodekX + R * (float)Math.Cos(Alfa1 + Dalfa * (i + 1)), SrodekY + R * (float)Math.Sin(Alfa1 + Dalfa * (i + 1)));
+            }
+        }
+
+        private void Draw16_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            float SrodekX = 250;
+            float SrodekY = 250;
+            float R = 200;
+            float Alfa = 0;
+            float Dalfa = (float)(2*Math.PI/20);
+            for (int i = 0; i<20; i++)
+            {
+                //g.DrawLine(pen1, SrodekX + R * (float)Math.Cos(Alfa + Dalfa * i), SrodekY + R * (float)Math.Sin(Alfa + Dalfa * i), SrodekX + R * (float)Math.Cos(Alfa + Dalfa * (i + 1)), SrodekY + R * (float)Math.Sin(Alfa + Dalfa * (i + 1)));
+                for (int j = 0; j < 20; j++)
+                {
+                    g.DrawLine(pen1, SrodekX + R * (float)Math.Cos(Alfa + Dalfa * i), SrodekY + R * (float)Math.Sin(Alfa + Dalfa * i), SrodekX + R * (float)Math.Cos(Alfa + Dalfa * j), SrodekY + R * (float)Math.Sin(Alfa + Dalfa * j));
+                }
+            }
+        }
+
+        private void Draw19_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            float size = 100;
+            float SrodekX = 250;
+            float SrodekY = 250;
+            float p1x = size/2;
+            float p1y = size / 2;
+            float p2x = size / 2;
+            float p2y = -size / 2;
+            float p3x = -size / 2;
+            float p3y = -size / 2;
+            float p4x = -size / 2;
+            float p4y = size / 2;
+            for(int i =0; i<100; i++)
+            {
+                g.DrawLine(pen1, p1x + SrodekX, p1y + SrodekY, p2x + SrodekX, p2y + SrodekY);
+                g.DrawLine(pen1, p2x + SrodekX, p2y + SrodekY, p3x + SrodekX, p3y + SrodekY);
+                g.DrawLine(pen1, p3x + SrodekX, p3y + SrodekY, p4x + SrodekX, p4y + SrodekY);
+                g.DrawLine(pen1, p1x + SrodekX, p1y + SrodekY, p4x + SrodekX, p4y + SrodekY);
+            }
+          
+        }
+    }
+}
